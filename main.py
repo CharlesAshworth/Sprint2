5eaf89344a9a041c57cfa06d972ce09ef2e1fc87
CharlesAshworth <60443039+CharlesAshworth@users.noreply.github.com>
2023-01-31 01:53:00 -0500

    Security Purposes

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+# Charles Ashworth
+#Professor Santore
+# COMP 490
+# Sprint 1
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+import secrets
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_wufoo_data() -> dict:
+    # URL Of the wufoo CUBES Project Proposal Submission
+    url = "http://charlieashworth23.wufoo.com/api/v3/forms/zv9axo1n8asf9/entries/json"
+    response = requests.get(url, auth=HTTPBasicAuth(secrets.wufoo_key, 'pass'))
+    print(response.text)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if response.status_code != 200:  # if we don't get an ok response we have trouble
+
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason}")
+        sys.exit(-1)
+    json_response = response.json()
+    return json_response
+
+## This will write the data to the file using the json library.
+def write_data_to_file():
+    data = get_wufoo_data()['Entries']
+
+    with open("w_data.txt", "w") as outfile:
+        for item in data:
+            for key, value in item.items():
+                outfile.write(f"{key}: {value}\n")
+
+    print_wfile("w_data.txt")
+
+    ## This will print the data from the file "w_data.txt"
+def print_wfile(file_name):
+    with open(file_name) as wufoo:
+        print(wufoo.read())
+
+
+if __name__ == "__main__":
+    write_data_to_file()
---
+++ readme.md
@@ -0,0 +1,9 @@
+(Sprint 1)
+This project was created by Charles Ashworth for COMP 490
+This project pulls the information from the Wufoo survey using an API
+There is an API key which will not be shared due to security purposes
+   - Pulls information
+   - Writes the pulled information in json to a "w_data.txt" file
+   - Reads the file in the console
+
+To my knowledge, there is nothing that this project is missing.
